96a89095fa00f26a6d3aa969b4a2c5bb3c24d37c
Luca <luca.mic92@gmail.com>
2021-06-10 22:48:38 +0200

    snack-2

---
+++ 2/js/script.js
@@ -0,0 +1,17 @@
+var parola1 = prompt('parola1');
+var parola2 = prompt('parola2');
+
+var risultato = prova(parola1, parola2);
+console.log(risultato);
+
+function prova(x,y){
+    var ris;
+    if (x.length ==y.length){
+        ris = 'sono uguali';
+    } else if (x.length > y.length){
+        ris = x;
+    } else {
+        ris = y;
+    }
+    return ris;
+}
